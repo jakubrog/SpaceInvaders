d53263deaa9945180df70f1da673c4e291d4f73b
Jakub Róg <jrog@student.agh.edu.pl>
2019-04-04 09:06:46 +0200

    added settings

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -9,6 +9,8 @@ FONT_PATH = BASE_PATH + '/fonts/'
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+ORANGE = (255, 156, 0)
+GRAY = (105, 105, 105)
 
 # names of created images
 IMG_NAMES = ['filename']
@@ -25,10 +27,11 @@ gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))  # Initia
 pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 
+MUSIC_ON = True
+
 
 # TODO: loading images into IMAGES
 # TODO: create images
-# TODO: create and implement main menu layout
 
 
 # single enemy ship
@@ -74,6 +77,45 @@ class Health(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
 
+def settings_menu_show():
+    font = pygame.font.Font(FONT, 35)
+    help_font = pygame.font.Font(FONT, 17)
+    about_font = pygame.font.Font(FONT, 25)
+
+    music_label = font.render("Music", True, RED)
+    help_label = help_font.render("[ESC] - get back", True, ORANGE)
+    music_select = font.render("< ON >", True, GRAY)
+    about = about_font.render("Created by Jakub Rog and Jan Makowiecki in 2019.", True, BLACK)
+    music = True
+
+    while True:
+        gameDisplay.fill(WHITE)
+        gameDisplay.blit(music_label, (50, 300))
+        gameDisplay.blit(music_select, (500, 300))
+        gameDisplay.blit(about, (105, 650))
+        gameDisplay.blit(help_label, (10, 730))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_RETURN]:
+                        if music:
+                            music_select = font.render("< OFF >", True, GRAY)
+                        else:
+                            music_select = font.render("< ON >", True, GRAY)
+                        music = not music
+
+                elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+                    gameDisplay.fill(WHITE)
+                    return music
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+
 class MainMenu:
     def __init__(self):
         gameDisplay.fill(WHITE)
@@ -84,7 +126,7 @@ class MainMenu:
         self.label2 = self.font.render("Settings", True, BLACK)
         self.label3 = self.font.render("Quit", True, BLACK)
 
-    def option_select(self, index):
+    def menu_option_select(self, index):
         self.label0 = self.font.render("New Game", True, BLACK)
         self.label1 = self.font.render("Highscores", True, BLACK)
         self.label2 = self.font.render("Settings", True, BLACK)
@@ -108,7 +150,10 @@ class MainMenu:
     # show - shows menu with buttons, returns selected options where 0 is start game and 2 is settings
     def show(self):
         index = 0
-        self.option_select(index)
+        self.menu_option_select(index)
+        music = True
+        level = 0
+        ship = 0
 
         while True:
             for event in pygame.event.get():
@@ -120,19 +165,26 @@ class MainMenu:
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
                     if index < 3:
                         index += 1
-                        self.option_select(index)
+                        self.menu_option_select(index)
 
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                     if index > 0:
                         index -= 1
-                        self.option_select(index)
+                        self.menu_option_select(index)
+
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                    #other menu choices
+                    # other menu choices
+                    if index == 0:
+                        ship, level = (1, 1)# after start there should be a function() -> tuple with ship and level
+                        return ship, level, music
+                    if index == 1:
+                        music = settings_menu_show()
+                        self.menu_option_select(index)
+
+                    # TODO: add showing highscores
                     if index == 3:
                         pygame.quit()
                         sys.exit()
-                    else:
-                        return index
 
             pygame.display.update()
             clock.tick(FPS)
@@ -151,14 +203,8 @@ class SpaceInvaders:
     # main function
     def main(self):
         while True:
-            action = self.menu.show()
-        
-            if action == 1:
-                continue  # TODO: move to second screen
-            if action == 2:
-                continue  # TODO: move to highscores
-            if action == 3:
-                continue  # TODO: move to settings
+            self.menu.show()
+            # menu
 
 
 if __name__ == '__main__':
