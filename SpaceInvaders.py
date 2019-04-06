ef16b7b1d7217babff1a8f484ef5e4fba18c02e5
jakubrog <jakubrog25@gmail.com>
2019-04-06 13:10:09 +0200

    ship can move and shoot, menu has background and images are loaded

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -13,7 +13,7 @@ ORANGE = (255, 156, 0)
 GRAY = (105, 105, 105)
 
 # names of created images
-IMG_NAMES = ['filename']
+IMG_NAMES = ['ship_1', 'bullet_1']
 
 FPS = 15
 
@@ -27,23 +27,20 @@ gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))  # Initia
 pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 
-MUSIC_ON = True
+IMAGES = {name: pygame.image.load(IMAGE_PATH + '{}.png'.format(name)).convert_alpha()
+          for name in IMG_NAMES}
 
 
-# TODO: loading images into IMAGES
-# TODO: create images
-
+# TODO: shrink images to reduce time of creating objects
+# TODO: ENEMIES: shoots, movement,
+# TODO: collisions
 
 # single enemy ship
 class Enemy(pygame.sprite.Sprite):  # sprite - base class for visible game objects
-    def __init__(self, row, column, image_filename, power):
+    def __init__(self):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
         # self.image = IMAGES['image_filename']
-        self.row = row
-        self.column = column
-        self.health = Health()
-        self.power = power
 
 
 # Block or a group of enemies, it's responsible for changing position of each enemy and select their specification
@@ -58,17 +55,44 @@ class GroupOfEnemies(pygame.sprite.Group):
 # Bullet - display and keep information about shot bullet
 # direction in constructor depends on who attack
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x_position, y_position, direction, speed, power):  # direction -
+    def __init__(self, x_position, y_position, direction, speed, image_name):  # direction -
         pygame.sprite.Sprite.__init__(self)
-        # self.image = IMAGES['filename']
+        self.image = IMAGES[image_name]
+        self.image = self.image = pygame.transform.scale(self.image, (10, 7))
+        self.rect = self.image.get_rect(topleft=(x_position, y_position))
         self.speed = speed
-        self.destruction = power
+        self.direction = direction
+
+    def update(self, keys, *args):
+        game.screen.blit(self.image, self.rect)
+        self.rect.y += self.speed * self.direction
+        if self.rect.y < 0 or self.rect.y > DISPLAY_HEIGHT:
+            self.kill()
 
 
 class Ship(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        # self.image = IMAGES['filename']
+        self.image = IMAGES['ship_1']
+        self.image = pygame.transform.scale(self.image, (70, 70))
+        self.rect = self.image.get_rect(topleft=(DISPLAY_WIDTH / 2 - 35, DISPLAY_HEIGHT - 80))
+        self.speed = 5
+
+    def update(self, keys):
+        gameDisplay.blit(self.image, self.rect)
+        if keys[pygame.K_LEFT] and self.rect.x > 10:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.x < DISPLAY_WIDTH - 80:
+            self.rect.x += self.speed
+
+    def shoot(self, bullets):
+        if len(bullets) < 2:  # max two bullets on screen at the same time
+            left_bullet = Bullet(self.rect.x + 3, self.rect.y + 10, -1, 15, 'bullet_1')
+            right_bullet = Bullet(self.rect.x + 57, self.rect.y + 10, -1, 15, 'bullet_1')
+            # self.sounds['shoot'].play()
+            bullets.add(left_bullet)
+            bullets.add(right_bullet)
+        return bullets
 
 
 # Health - displaying HP point and health management
@@ -77,7 +101,7 @@ class Health(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
 
-def settings_menu_show():
+def settings_menu_show(background):
     font = pygame.font.Font(FONT, 35)
     help_font = pygame.font.Font(FONT, 17)
     about_font = pygame.font.Font(FONT, 25)
@@ -85,11 +109,11 @@ def settings_menu_show():
     music_label = font.render("Music", True, RED)
     help_label = help_font.render("[ESC] - get back", True, ORANGE)
     music_select = font.render("< ON >", True, GRAY)
-    about = about_font.render("Created by Jakub Rog and Jan Makowiecki in 2019.", True, BLACK)
+    about = about_font.render("Created by Jakub Rog and Jan Makowiecki in 2019.", True, WHITE)
     music = True
 
     while True:
-        gameDisplay.fill(WHITE)
+        gameDisplay.blit(background, (0, 0))
         gameDisplay.blit(music_label, (50, 300))
         gameDisplay.blit(music_select, (500, 300))
         gameDisplay.blit(about, (105, 650))
@@ -102,14 +126,14 @@ def settings_menu_show():
 
             if event.type == pygame.KEYDOWN:
                 if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_RETURN]:
-                        if music:
-                            music_select = font.render("< OFF >", True, GRAY)
-                        else:
-                            music_select = font.render("< ON >", True, GRAY)
-                        music = not music
+                    if music:
+                        music_select = font.render("< OFF >", True, GRAY)
+                    else:
+                        music_select = font.render("< ON >", True, GRAY)
+                    music = not music
 
-                elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
-                    gameDisplay.fill(WHITE)
+                elif event.key == pygame.K_q or event.key in [pygame.K_ESCAPE, pygame.K_BACKSPACE]:
+                    gameDisplay.fill(BLACK)
                     return music
 
         pygame.display.update()
@@ -118,19 +142,21 @@ def settings_menu_show():
 
 class MainMenu:
     def __init__(self):
-        gameDisplay.fill(WHITE)
+        self.background = pygame.image.load(IMAGE_PATH + "menu.png").convert()
+        self.background = pygame.transform.scale(self.background, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+        gameDisplay.blit(self.background, (0, 0))
         self.font = pygame.font.Font(FONT, 25)
         self.index = 0
         self.label0 = self.font.render("New Game", True, RED)
-        self.label1 = self.font.render("Highscores", True, BLACK)
-        self.label2 = self.font.render("Settings", True, BLACK)
-        self.label3 = self.font.render("Quit", True, BLACK)
+        self.label1 = self.font.render("Highscores", True, WHITE)
+        self.label2 = self.font.render("Settings", True, WHITE)
+        self.label3 = self.font.render("Quit", True, WHITE)
 
     def menu_option_select(self, index):
-        self.label0 = self.font.render("New Game", True, BLACK)
-        self.label1 = self.font.render("Highscores", True, BLACK)
-        self.label2 = self.font.render("Settings", True, BLACK)
-        self.label3 = self.font.render("Quit", True, BLACK)
+        self.label0 = self.font.render("New Game", True, WHITE)
+        self.label1 = self.font.render("Highscores", True, WHITE)
+        self.label2 = self.font.render("Settings", True, WHITE)
+        self.label3 = self.font.render("Quit", True, WHITE)
 
         if index == 0:
             self.label0 = self.font.render("> New Game", True, RED)
@@ -141,7 +167,7 @@ class MainMenu:
         elif index == 3:
             self.label3 = self.font.render("> Quit", True, RED)
 
-        gameDisplay.fill(WHITE)
+        gameDisplay.blit(self.background, (0, 0))
         gameDisplay.blit(self.label0, (100, 520))
         gameDisplay.blit(self.label1, (100, 570))
         gameDisplay.blit(self.label2, (100, 620))
@@ -175,10 +201,12 @@ class MainMenu:
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                     # other menu choices
                     if index == 0:
+                        gameDisplay.blit(self.background, (0, 0))
                         ship, level = (1, 1)  # after start there should be a function() -> tuple with ship and level
                         return ship, level, music
-                    if index == 1:
-                        music = settings_menu_show()
+
+                    if index == 2:
+                        music = settings_menu_show(self.background)
                         self.menu_option_select(index)
 
                     # TODO: add showing highscores
@@ -195,15 +223,37 @@ class SpaceInvaders:
     def __init__(self):
         self.screen = gameDisplay
         self.menu = MainMenu()
+        self.ship = Ship()
+        self.allSprites = pygame.sprite.Group(self.ship)  # every item which is on screen should be here
+        self.map = pygame.image.load(IMAGE_PATH + 'map1.png')
+        self.bullets = pygame.sprite.Group()  # every bullet is here and whole group is in allSpirites
+
+        # self.map = pygame.transform.scale(self.map, (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
     # main loop
     # def gameLoop:
     # while True:
 
     # main function
     def main(self):
+        ship, diff_level, music = self.menu.show()
         while True:
-            self.menu.show()
-            # menu
+            gameDisplay.blit(self.map, (0, 0))  # map load
+            keys = pygame.key.get_pressed()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.bullets = self.ship.shoot(self.bullets)
+                        self.allSprites.add(self.bullets)
+
+            self.allSprites.update(keys)  # update every element
+
+            pygame.display.update()
+            clock.tick(60)
 
 
 if __name__ == '__main__':
